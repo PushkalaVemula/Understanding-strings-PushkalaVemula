7740005a95cf889fce37f391fa344d870703c585
321910401013 gitam <321910401013@gitam.in>
2020-10-24 13:41:25 +0000

    Done

---
+++ placeholders.py
@@ -0,0 +1,16 @@
+  
+#  *****   DO NOT MODIFY THIS FILE. *****
+#
+#
+#
+# THIS IS A HELPER UTILITY USED IN LESSONS AND ASSIGNMENTS
+# IT EXPORTS A FEW CONSTANTS WHICH ARE USED TO ENSURE THAT LESSONS AND ASSIGNMENTS
+# DO NOT SHOW COMPILATION ERRORS.
+
+__author__ = 'Ramesh Naik'
+
+
+__all__ = ["__", "___"]
+
+__ = "-- Replace me with expected value or True/False --"
+___ = 0  # Replace me with numeric values
